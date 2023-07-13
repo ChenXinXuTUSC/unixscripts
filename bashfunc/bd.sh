011d39abcbdc52936e30ca96464a474cb0cff912
chenxinxu <fredomfu@gmail.com>
2023-07-13 22:49:57 +0800

    fix: use exit instead of return to end shell script.

--- bashfunc/bd.sh
+++ bashfunc/bd.sh
@@ -4,14 +4,15 @@ bcd()
 {
 	if [ ! -n "${1}" ]; then
 		echo "please specify backward level <num>"
-		return 0
+		exit -1
 	fi
-	
-	updir="../"
-	for ((i = 1; i < ${1}; i++)); do
-		updir="${updir}${updir}";
-	done
-	cd ${updir}
 
-	return 0
+	updir="../"
+	for((i = 1; i < ${1}; i++)); do
+		updir="${updir}${updir}"
+	done
+
+	cd ${updir}
 }
+
+alias bd='bcd'
