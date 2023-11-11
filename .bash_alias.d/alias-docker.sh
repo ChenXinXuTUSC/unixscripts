7296d072338cc9d7726473401c780f8e33797f2a
chenxinxu <1031307782@qq.com>
2023-11-11 19:51:26 +0800

    misc: alias for docker ps -a.

--- .bash_alias.d/alias-docker.sh
+++ .bash_alias.d/alias-docker.sh
@@ -4,6 +4,7 @@ if command -v docker > /dev/null; then
     # alias docker='dck'
     alias dckimg='docker image ls'
     alias dckctn='docker container ls'
+    alias dckpsa='docker ps -a'
     alias dckrmi='docker image rm'
     alias dckrmd='docker image rm $(docker images -f "dangling=true" -q)'
 fi
